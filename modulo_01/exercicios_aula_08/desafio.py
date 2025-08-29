2217024886f23982582f8999dd0489272f61f174
Anderson Rodrigues <anderson.c.rodrigues@hotmail.com>
2025-08-29 14:30:22 -0300

    chore: tratamento de erro para entrada de valores

--- modulo_01/exercicios_aula_08/desafio.py
+++ modulo_01/exercicios_aula_08/desafio.py
@@ -1,36 +1,24 @@
-lA = input("Entrada do lado A: ")
-lB = input("Entrada do lado B: ")
-lC = input("Entrada do lado C: ")
+while True:
+    try:
+        lA = float(input("Digite o valor do lado A: "))
+        lB = float(input("Digite o valor do lado B: "))
+        lC = float(input("Digite o valor do lado C: "))
 
-if lA.isdigit() and lB.isdigit() and lC.isdigit():
-    lA = int(lA)
-    lB = int(lB)
-    lC = int(lC)
+        if lA > 0 and lB > 0 and lC > 0:
+            if (lA < lB + lC) and (lB < lA + lC) and (lC < lA + lB):
+                print("\nOs valores podem formar um triângulo.")
 
-    if lA <= 0 or lB <= 0 or lC <= 0:
-        print("Entradas inválidas. Os lados devem ser números positivos.")
-        exit()
-else:
-    print("Entradas inválidas. Por favor, insira números inteiros positivos.")
-    exit()
+                if lA == lB == lC:
+                    print("Tipo: Triângulo Equilátero.")
+                elif lA == lB or lA == lC or lB == lC:
+                    print("Tipo: Triângulo Isósceles.")
+                else:
+                    print("Tipo: Triângulo Escaleno.")
+                break 
+            else:
+                print("Erro: os valores não podem formar um triângulo.\n")
+        else:
+            print("Erro: os lados devem ser maiores que zero.\n")
 
-soma_ok = True
-if lA > lB + lC:
-    soma_ok = False
-if lB > lA + lC:
-    soma_ok = False
-if lC > lA + lB:
-    soma_ok = False
-
-dif_ok = True
-if lA < abs(lB - lC):
-    dif_ok = False
-if lB < abs(lA - lC):
-    dif_ok = False
-if lC < abs(lA - lB):
-    dif_ok = False
-
-if soma_ok and dif_ok:
-    print(f"Os lados {lA}, {lB} e {lC} podem formar um triângulo.")
-else:
-    print(f"Os lados {lA}, {lB} e {lC} não podem formar um triângulo.")
+    except ValueError:
+        print("Erro: entrada inválida! Digite apenas números (inteiros ou decimais).\n")
