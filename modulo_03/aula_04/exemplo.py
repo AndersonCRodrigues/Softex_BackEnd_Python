f9805beb1547460795aa1a97f00fab93adc8b122
Anderson Costa Rodrigues <110496377+AndersonCRodrigues@users.noreply.github.com>
2025-10-13 16:05:02 -0300

    feat: explicando contatenação de strings

--- modulo_03/aula_04/exemplo.py
+++ modulo_03/aula_04/exemplo.py
@@ -1,3 +1,11 @@
-lista = ["email = ?", "senha = ?"]
+updates = ["email = ?", "senha = ?", "data_atualizacao = ?"]
+query = f"UPDATE usuarios SET {', '.join(updates)} WHERE id = ?;"
 
-print(", ".join(lista))
+print(query)
+
+
+f"""
+litsa de strings:
+{updates}
+final da minha string!
+"""
