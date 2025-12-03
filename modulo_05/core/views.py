551685af18d86664447a51bc61c25497561f6a6a
Anderson Costa Rodrigues <110496377+AndersonCRodrigues@users.noreply.github.com>
2025-12-03 10:25:58 -0300

    bomba 2

--- modulo_05/core/views.py
+++ modulo_05/core/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Tarefa
+from .serializers import TarefaSerializer
 
-# Create your views here.
+class ListaTarefasAPIView(APIView):
+
+    def get(self, request, format=None):
+        tarefas = Tarefa.objects.all()
+        serializer = TarefaSerializer(tarefas, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
