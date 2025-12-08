e91cb9c6ecfd848174dd572be8b1f5601c659646
Anderson Costa Rodrigues <110496377+AndersonCRodrigues@users.noreply.github.com>
2025-12-08 10:26:58 -0300

    feat: metodo post

--- modulo_05/core/views.py
+++ modulo_05/core/views.py
@@ -10,3 +10,17 @@ class ListaTarefasAPIView(APIView):
         tarefas = Tarefa.objects.all()
         serializer = TarefaSerializer(tarefas, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def post(self, request, format=None):
+
+        serializer = TarefaSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(
+                serializer.data,
+                status=status.HTTP_201_CREATED
+            )
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+        )
