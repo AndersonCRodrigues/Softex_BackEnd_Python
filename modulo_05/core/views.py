863743ab252ed77d950a663a49b631be90cffdce
Anderson Costa Rodrigues <110496377+AndersonCRodrigues@users.noreply.github.com>
2025-12-10 11:26:26 -0300

    ferat: get-put-path-delete de tarefas

--- modulo_05/core/views.py
+++ modulo_05/core/views.py
@@ -5,6 +5,8 @@ from .models import Tarefa
 from .serializers import TarefaSerializer
 from django.db import IntegrityError
 import logging
+from django.shortcuts import get_object_or_404 
+
 logger = logging.getLogger(__name__)
 
 
@@ -46,3 +48,45 @@ class ListaTarefasAPIView(APIView):
                 status=status.HTTP_500_INTERNAL_SERVER_ERROR
             )
 
+
+class DetalheTarefaAPIView(APIView): 
+    def get_object(self, pk): 
+        return get_object_or_404(Tarefa, pk=pk)
+    
+    def get(self, request, pk, format=None): 
+        tarefa = self.get_object(pk) 
+        serializer = TarefaSerializer(tarefa) 
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def put(self, request, pk, format=None): 
+        
+        tarefa = self.get_object(pk) 
+        serializer = TarefaSerializer(tarefa, data=request.data) 
+    
+        if serializer.is_valid(): 
+            serializer.save() 
+            
+            return Response(serializer.data, status=status.HTTP_200_OK) 
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST) 
+    
+    def patch(self, request, pk, format=None): 
+
+        tarefa = self.get_object(pk) 
+        serializer = TarefaSerializer( 
+            tarefa, 
+            data=request.data, 
+            partial=True
+        ) 
+ 
+        if serializer.is_valid(): 
+            serializer.save() 
+ 
+            return Response(serializer.data, status=status.HTTP_200_OK) 
+ 
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request, pk, format=None): 
+
+        tarefa = self.get_object(pk) 
+        tarefa.delete() 
+        return Response(status=status.HTTP_204_NO_CONTENT)
